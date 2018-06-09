c6e7a9c6a5e5465b2325574f4660ff499e57fa3f
Eimantas <noreika.eimantas@gmail.com>
2018-06-09 17:06:03 +0300

    Added time showing

--- SmartMirrorForAndroid/MainActivity.cs
+++ SmartMirrorForAndroid/MainActivity.cs
@@ -2,18 +2,36 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Views;
+using System;
+using System.Threading.Tasks;
 
 namespace SmartMirrorForAndroid
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape, Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+
+        TextView clock;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
+            //Fullscreen
+            this.Window.AddFlags(WindowManagerFlags.Fullscreen);
             SetContentView(Resource.Layout.activity_main);
+
+            clock = FindViewById<TextView>(Resource.Id.textView2);
+        }
+
+        protected async override void OnResume()
+        {
+            base.OnResume();
+        }
+
+
+        public static void GetTemperature()
+        {
+
         }
     }
 }
